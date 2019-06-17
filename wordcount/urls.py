f3877d3a65e1cead3097c122e1f9e6e3673bf0e2
chengirls <2233979918@qq.com>
2019-06-17 16:02:44 +0800

    词频

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -20,5 +20,7 @@ from wordcount import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('count/',views.count)
 ]
+
--- wordcount/views.py
+++ wordcount/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 #     return HttpResponse('hello')
 def home(request):
     return render(request,'home.html')
+
+
+def count(request):
+    text=request.GET['text']
+    print(text)
+    result={}
+    for i in text:
+        if i not in result:
+            result[i]=1
+        else:
+            result[i]+=1
+    print(result)
+    result=sorted(result.items(),key=lambda x:x[1],reverse=True)
+    return render(request,'count.html',{'count_result':result.items()})
